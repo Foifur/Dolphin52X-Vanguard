a3b343d81a200f6e5cf60e82a9a9ff0c264e6b9e
Foifur <jqgreenan@gmail.com>
2026-08-04 10:40:48 -0700

    Use lightweight tags instead of annotated

--- Source/Core/Common/make_scmrev.h.js
+++ Source/Core/Common/make_scmrev.h.js
@@ -3,10 +3,10 @@ var oFS				= new ActiveXObject("Scripting.FileSystemObject");
 
 var outfile			= "./scmrev.h";
 var cmd_revision	= " rev-parse HEAD";
-var cmd_describe	= " describe --always --long --dirty";
+var cmd_describe = " describe --always --long --tags --dirty ";
 var cmd_branch		= " rev-parse --abbrev-ref HEAD";
 var cmd_commits_ahead = " rev-list --count HEAD ^master";
-var cmd_get_tag       = " describe --exact-match HEAD";
+var cmd_get_tag       = " describe --exact-match --tags HEAD";
 
 function GetGitExe()
 {
